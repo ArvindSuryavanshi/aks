6e88ee28d1c2507335aba743bc797a8ba5e1fd0a
ArvindSuryavanshi <your-email@gmail.com>
2026-03-11 23:47:05 +0530

    Initial commit for Spring Boot Starter Dependencies example

---
+++ src/main/java/com/product/Application.java
@@ -0,0 +1,23 @@
+package com.product;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("Spring Boot 3 Application Started");
+
+        ProductService service = new ProductService();
+        service.processProduct();
+
+    }
+}
---
+++ src/main/java/com/product/ProductService.java
@@ -0,0 +1,12 @@
+package com.product;
+
+public class ProductService {
+
+    public void processProduct() {
+
+        System.out.println("Processing product data...");
+        System.out.println("Business logic executed successfully");
+
+    }
+
+}
