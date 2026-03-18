854a8213639d98103f158801d94ab9b87066f8b9
ArvindSuryavanshi <arvindsinghmca82@gmail.com>
2026-03-18 22:39:15 +0530

    01=FirstSpringbootApplication

---
+++ 01=FirstSpringbootApplication/main/java/org/prospringboot/FirstSpringbootApplication.java
@@ -0,0 +1,16 @@
+package org.prospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FirstSpringbootApplication.class, args);
+
+        System.out.println("Spring Boot Application Started...");
+    }
+
+}
